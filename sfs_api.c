e6ed7134e34aae1a5c18e80ecfc14205cb7ddae8
Julian Armour <julian.armour@videotron.ca>
2019-11-16 17:32:32 -0500

    inodeTbl_init()

--- sfs_api.c
+++ sfs_api.c
@@ -4,25 +4,33 @@
 #include <string.h>
 
 //function declarations
-void inodeTbl_init();
+static void inodeTbl_init();
+
+static void openFileTbl_init();
+
+static const int MAXFNAMESIZE = 20;//maximum length of a file name (including 'period' and 'file extension'
+static const int BLOCKSIZE = 1024;//size in bytes of a block
+static const int MAXBLOCK = 256;//number of disk blocks
+static const int INODEBLKS = 1;//number of "inode table" blocks (up to 255 files supported)
+static const int INODEBLK = 1;//the Inode Table's block address
+static const int FREEBMBLKS = 1;//number of "free bitmap" blocks
+static const int ROOTDIRINODE = 0;//the inode id (index in the inode tbl) for the root directory
 
 enum mode {MODE_DIR, MODE_BASIC};
-
-const int BLOCKSIZE = 1024;// size in bytes of a block
-const int MAXBLOCK = 256;//number of disk blocks
-const int INODEBLKS = 1;//number of "inode table" blocks
-const int FREEBMBLKS = 1;//number of "free bitmap" blocks
-const int ROOTDIRINODE = 0;//the inode id (index in the inode tbl) for the root directory
+typedef char Dir_Entry[MAXFNAMESIZE + sizeof(int)];//an entry in the root directory [filename|inode]
+typedef struct {int inode; int read; int write;} FD;//a file descriptor
 
 //In-memory data structures
-int inodetbl[256];// Inode Table Cache
-
+int inodeTbl[256];//Inode Table cache (holds up to 256 inodes)
+Dir_Entry dir[256];//Directory cache. (holds up to 256 files)
+FD oft[256];//Open File Descriptor Table (holds up to 256 open files)
+int freeMap[16];//Free Block Bitmap
 
 void mksfs(int fresh) {
   //disk size: 256KiB (256 blocks)
-  //max file size: 268 KiB (limited by disk size)
+  //max file size: 268 KiB (limited by disk size of course)
   //max number of files: 256
-  //DISK STRUCTURE: [SUPER(1 block)|I-NODES(1)|FREE-BITMAP(1)|DATA-BLOCKS(253)]
+  //DISK STRUCTURE: [SUPER(1 block)|INODE-TBL(1)|FREE-BITMAP(1)|DATA-BLOCKS(253)]
   //INODE STRUCTURE: [mode|size|pointer1|...|pointer12|ind-pointer]
   int blockBuff[BLOCKSIZE/4];//temp buffer for writing blocks at FS creation
   if (fresh) {//insert initial filesystem data
@@ -35,20 +43,21 @@ void mksfs(int fresh) {
     blockBuff[4] = ROOTDIRINODE;// root directory inode index
     write_blocks(0, 1, blockBuff);//set super block
     memset(blockBuff, 0, BLOCKSIZE);//reset blockBuff
-    //init free bitmap
+    //set bits in free bitmap
     //1 super + 1 inode tbl block + 1 free bitmap block + 1 inode (for root dir)
-    //+ 1 (root dir) = 5 blocks
-    blockBuff[0] = 0xF8000000;//reserve first 5 blocks
+    //+ 1 (root dir data) = 5 blocks
+    //reserve first 5 blocks
+    blockBuff[0] = -134217728;//(Decimal representation of 0xF8000000, or equivalently, 0b1111100...0)
     write_blocks(2, 1, blockBuff);//set free bitmap
     memset(blockBuff, 0, BLOCKSIZE);//reset blockBuff
-    //init root directory inode
+    //init root directory's inode
     blockBuff[0] = MODE_DIR;
-    blockBuff[2] = 4;
+    blockBuff[2] = 4;//initial data block for root dir
     write_blocks(3, 1, blockBuff);//set root dir inode
     memset(blockBuff, 0, BLOCKSIZE);//reset blockBuff
-    //set root directory’ inode in inode table
-    blockBuff[0] = 3;
-    write_blocks(1, 1, blockBuff);
+    //add root directory’s inode in inode table
+    blockBuff[0] = 3;//point root dir's inode #0 to block #3
+    write_blocks(INODEBLK, 1, blockBuff);
     memset(blockBuff, 0, BLOCKSIZE);//reset blockBuff
   } else {
     init_disk("sfs", BLOCKSIZE, MAXBLOCK);
@@ -60,6 +69,11 @@ void mksfs(int fresh) {
   freeBitmap_init();//loads the Free Data Block Bitmap into memory (cache)
 }
 
-void inodeTbl_init() {
+static void openFileTbl_init() {
 
+}
+
+static void inodeTbl_init() {
+  memset(inodeTbl, 0, sizeof(inodeTbl));
+  read_blocks(INODEBLK, 1, inodeTbl);
 }
