acdd2550713cf38f334350401813e70d616b1f87
Julian Armour <julian.armour@videotron.ca>
2019-11-17 21:26:18 -0500

    refactor block count and block size constant names

--- sfs_api.c
+++ sfs_api.c
@@ -1,9 +1,13 @@
 /*
  * Simple File System
  *
- * Assumptions:
- *  - sizeof(int) = 32
+ * Author: Julian Armour
  *
+ * disk size: 256KiB (256 blocks)
+ * max file size: 268 KiB (limited by disk size of course)
+ * max number of files: 256
+ * DISK STRUCTURE: [SUPER(1 block)|INODE-TBL(1)|FREE-BITMAP(1)|DATA-BLOCKS(253)]
+ * INODE STRUCTURE: [mode|size|pointer1|...|pointer12|ind-pointer]
  */
 
 #include "sfs_api.h"
@@ -12,15 +16,15 @@
 #include <string.h>
 
 static const int MAX_FNAME_SIZE = 20;//maximum length of a file name (including 'period' and 'file extension'
-static const int BLOCK_SIZE = 1024;//size in bytes of a block
-static const int MAX_BLOCK = 256;//number of disk blocks
+static const int BLOCK_BYTES = 1024;//size in bytes of a block
+static const int BLOCK_COUNT = 256;//number of disk blocks
 static const int INODE_BLKS = 1;//number of "inode table" blocks
 static const int INODE_BLK = 1;//the Inode Table's block address
 static const int FREE_BM_BLKS = 1;//number of "free bitmap" blocks
 static const int FREE_BM_BLK = 2;//the free block bitmap's block address
 static const int ROOT_DIR_INODE = 0;//the inode id (index in the inode tbl) for the root directory
 static const int MAX_FILES = 256;//maximum number of files sfs can create (including root)
-static const int MAX_FILE_SIZE = BLOCK_SIZE * 268;//inode can hold 268 data blocks
+static const int MAX_FILE_SIZE = BLOCK_BYTES * 268;//inode can hold 268 data blocks
 
 enum mode {MODE_DIR, MODE_BASIC};
 typedef char DirEntry[MAX_FNAME_SIZE + sizeof(int)];//an entry in the root directory [filename|inodeId]
@@ -57,35 +61,35 @@ void mksfs(int fresh) {
   //max number of files: 256
   //DISK STRUCTURE: [SUPER(1 block)|INODE-TBL(1)|FREE-BITMAP(1)|DATA-BLOCKS(253)]
   //INODE STRUCTURE: [mode|size|pointer1|...|pointer12|ind-pointer]
-  int blockBuff[BLOCK_SIZE / 4];//temp buffer for writing blocks at FS creation
+  int blockBuff[BLOCK_BYTES / 4];//temp buffer for writing blocks at FS creation
   if (fresh) {//insert initial filesystem data
-    init_fresh_disk("sfs", BLOCK_SIZE, MAX_BLOCK);
+    init_fresh_disk("sfs", BLOCK_BYTES, BLOCK_COUNT);
     //init super block
-    blockBuff[0] = BLOCK_SIZE;// size in bytes of a block
-    blockBuff[1] = MAX_BLOCK;//number of filesystem blocks
+    blockBuff[0] = BLOCK_BYTES;// size in bytes of a block
+    blockBuff[1] = BLOCK_COUNT;//number of filesystem blocks
     blockBuff[2] = INODE_BLKS;//number of "inode table" blocks
     blockBuff[3] = FREE_BM_BLKS;//number of "free bitmap" blocks
     blockBuff[4] = ROOT_DIR_INODE;// root directory inode index
     write_blocks(0, 1, blockBuff);//set super block
-    memset(blockBuff, 0, BLOCK_SIZE);//reset blockBuff
+    memset(blockBuff, 0, BLOCK_BYTES);//reset blockBuff
     //set bits in free bitmap
     //1 super + 1 inode tbl block + 1 free bitmap block + 1 inode (for root dir)
     //+ 1 (root dir data) = 5 blocks
     //reserve first 5 blocks
     blockBuff[0] = -134217728;//(Decimal representation of 0xF8000000, or equivalently, 0b1111100...0)
     write_blocks(FREE_BM_BLK, 1, blockBuff);//set free bitmap
-    memset(blockBuff, 0, BLOCK_SIZE);//reset blockBuff
+    memset(blockBuff, 0, BLOCK_BYTES);//reset blockBuff
     //init root directory's inode
     blockBuff[0] = MODE_DIR;
     blockBuff[2] = 4;//initial data block for root dir
     write_blocks(3, 1, blockBuff);//set root dir inode
-    memset(blockBuff, 0, BLOCK_SIZE);//reset blockBuff
+    memset(blockBuff, 0, BLOCK_BYTES);//reset blockBuff
     //add root directory’s inode in inode table
     blockBuff[0] = 3;//point root dir's inode #0 to block #3
     write_blocks(INODE_BLK, 1, blockBuff);
-    memset(blockBuff, 0, BLOCK_SIZE);//reset blockBuff
+    memset(blockBuff, 0, BLOCK_BYTES);//reset blockBuff
   } else {
-    init_disk("sfs", BLOCK_SIZE, MAX_BLOCK);
+    init_disk("sfs", BLOCK_BYTES, BLOCK_COUNT);
   }
   inodeTbl_init();//loads inode table into memory (cache)
   //load an open file descriptor table, with only the root DIR opened at index 0.
@@ -113,8 +117,8 @@ int sfs_fread(int fileID, char *buf, int length) {
   int bufIndex = 0;
   while (bufIndex < length) {
     int blockNum;//address of block being written to
-    char blockBuff[BLOCK_SIZE];//buffer for data block
-    int inodePointer = file.read / BLOCK_SIZE;
+    char blockBuff[BLOCK_BYTES];//buffer for data block
+    int inodePointer = file.read / BLOCK_BYTES;
     //get blockNum for inodePointer, it will be <= 0 if it's not allocated
     if (inodePointer < 12) {//non-indirect pointer
       blockNum = inode.pointers[inodePointer];
@@ -131,9 +135,9 @@ int sfs_fread(int fileID, char *buf, int length) {
     }
     /*now perform read*/
     //where the read pointer is within the block
-    int blockReadPointer = file.read % BLOCK_SIZE;
+    int blockReadPointer = file.read % BLOCK_BYTES;
     //read until either end of block or end of buffer
-    int numBytes = min(BLOCK_SIZE - blockReadPointer, length - bufIndex);
+    int numBytes = min(BLOCK_BYTES - blockReadPointer, length - bufIndex);
     if (blockNum <= 0) {
       //no data block, treat as all-zero block
       memset(&buf[bufIndex], 0, numBytes);
@@ -152,7 +156,7 @@ int sfs_fread(int fileID, char *buf, int length) {
 
 /*Updates the on-disk inode data-structure with in-memory inode.*/
 void flushInode(int inodeID, Inode inode) {
-  int blk[BLOCK_SIZE / sizeof(int)];
+  int blk[BLOCK_BYTES / sizeof(int)];
   int inodeBlkAddr = inodeTbl[inodeID];
   read_blocks(inodeBlkAddr, 1, (char *) blk);
   //encode inode to block
@@ -175,8 +179,8 @@ int sfs_fwrite(int fileID, char *buf, int length) {
   int bufIndex = 0;
   while (bufIndex < length) {
     int blockNum;//address of block being written to
-    char blockBuff[BLOCK_SIZE];//buffer for Data Block
-    int inodePointer = file.write / BLOCK_SIZE;
+    char blockBuff[BLOCK_BYTES];//buffer for Data Block
+    int inodePointer = file.write / BLOCK_BYTES;
     //get blockNum for inodePointer, allocate blocks as needed
     if (inodePointer < 12) {//none-indirect pointer
       if (inode.pointers[inodePointer] <= 0)//no block already allocated
@@ -202,11 +206,11 @@ int sfs_fwrite(int fileID, char *buf, int length) {
     /*now perform write*/
 
     //where the write pointer is within the block
-    int blockWritePointer = file.write % BLOCK_SIZE;
+    int blockWritePointer = file.write % BLOCK_BYTES;
     //number of bytes to write, write until either end of block or end of buffer
-    int numBytes = min(BLOCK_SIZE - blockWritePointer, length - bufIndex);
+    int numBytes = min(BLOCK_BYTES - blockWritePointer, length - bufIndex);
     //only read existing block if we don't overwrite the entire block
-    if (numBytes < BLOCK_SIZE)
+    if (numBytes < BLOCK_BYTES)
       read_blocks(blockNum, 1, blockBuff);
     memcpy(&blockBuff[blockWritePointer], &buf[bufIndex], numBytes);
     write_blocks(blockNum, 1, blockBuff);
@@ -278,7 +282,7 @@ static void openFileTbl_init() {
 /*Given an index in the inode table (inodeId), this will return the corresponding inode data-structure,*/
 static Inode fetchInode(int inodeId) {
   Inode inode;
-  int blk[BLOCK_SIZE/4];
+  int blk[BLOCK_BYTES / 4];
   read_blocks(inodeTbl[inodeId], 1, blk);
   //parse inode block
   inode.mode = blk[0];
